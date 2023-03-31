79a2c4a73b54f620308e57edb506bcec2d8054f8
Dan <danporeda@yahoo.com>
2023-03-30 20:17:20 -0400

    template contracts

---
+++ contracts/DecentralizedFreePlaylist.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IPlayToken is IERC20 {
+    function mint(address to, uint256 amount) external;
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+interface IMyNFT {
+    function safeMint(address to, uint256 tokenId, string memory uri) external;
+}
+
+
+contract FreeDecentralizedPlaylist is Ownable {
+    // TODO: create project!
+}
